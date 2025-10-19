203881167fcffe277b45fabec98608c36303624e
dtrthg233 <dtrthg233@gmail.com>
2025-10-20 05:08:37 +0800

    meow11

--- .vitepress/theme/assets/themeConfig.mjs
+++ .vitepress/theme/assets/themeConfig.mjs
@@ -143,78 +143,78 @@ export const themeConfig = {
     },
   ],
   // 导航栏菜单 - 左侧
-  navMore: [
-    {
-      name: "博客",
-      list: [
-        {
-          icon: "/images/logo/mfy_welcome.png",
-          name: "主站",
-          url: "/",
-        },
-        {
-          icon: "/images/logo/mfy_welcome.png",
-          name: "博客镜像站",
-          url: "https://blog-backup.imsyy.top/",
-        },
-      ],
-    },
-    {
-      name: "服务",
-      list: [
-        {
-          icon: "https://pic.efefee.cn/uploads/2024/04/08/6613465358077.png",
-          name: "起始页",
-          url: "https://nav.imsyy.top/",
-        },
-        {
-          icon: "https://pic.efefee.cn/uploads/2024/04/08/661346d418ad7.png",
-          name: "今日热榜",
-          url: "https://hot.imsyy.top/",
-        },
-        {
-          icon: "https://pic.efefee.cn/uploads/2024/04/08/66134722586fa.png",
-          name: "站点监测",
-          url: "https://status.imsyy.top/",
-        },
-      ],
-    },
-    {
-      name: "项目",
-      list: [
-        {
-          icon: "/images/logo/mfy_welcome.png",
-          name: "Curve",
-          url: "https://github.com/imsyy/vitepress-theme-curve",
-        },
-        {
-          icon: "https://pic.efefee.cn/uploads/2024/04/07/66124f5fc63c8.png",
-          name: "SPlayer",
-          url: "https://github.com/imsyy/SPlayer",
-        },
-        {
-          icon: "https://pic.efefee.cn/uploads/2024/04/08/6613465358077.png",
-          name: "Snavigation",
-          url: "https://github.com/imsyy/SPlayer",
-        },
-        {
-          icon: "/images/logo/mfy_welcome.png",
-          name: "Home",
-          url: "https://github.com/imsyy/home",
-        },
-        {
-          icon: "https://pic.efefee.cn/uploads/2024/04/08/661346d418ad7.png",
-          name: "DailyHotApi",
-          url: "https://github.com/imsyy/DailyHotApi",
-        },
-        {
-          icon: "https://pic.efefee.cn/uploads/2024/04/08/66134722586fa.png",
-          name: "site-status",
-          url: "https://github.com/imsyy/site-status",
-        },
-      ],
-    },
-  ],
+  // navMore: [
+  //   {
+  //     name: "博客",
+  //     list: [
+  //       {
+  //         icon: "/images/logo/mfy_welcome.png",
+  //         name: "主站",
+  //         url: "/",
+  //       },
+  //       {
+  //         icon: "/images/logo/mfy_welcome.png",
+  //         name: "博客镜像站",
+  //         url: "https://blog-backup.imsyy.top/",
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     name: "服务",
+  //     list: [
+  //       {
+  //         icon: "https://pic.efefee.cn/uploads/2024/04/08/6613465358077.png",
+  //         name: "起始页",
+  //         url: "https://nav.imsyy.top/",
+  //       },
+  //       {
+  //         icon: "https://pic.efefee.cn/uploads/2024/04/08/661346d418ad7.png",
+  //         name: "今日热榜",
+  //         url: "https://hot.imsyy.top/",
+  //       },
+  //       {
+  //         icon: "https://pic.efefee.cn/uploads/2024/04/08/66134722586fa.png",
+  //         name: "站点监测",
+  //         url: "https://status.imsyy.top/",
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     name: "项目",
+  //     list: [
+  //       {
+  //         icon: "/images/logo/mfy_welcome.png",
+  //         name: "Curve",
+  //         url: "https://github.com/imsyy/vitepress-theme-curve",
+  //       },
+  //       {
+  //         icon: "https://pic.efefee.cn/uploads/2024/04/07/66124f5fc63c8.png",
+  //         name: "SPlayer",
+  //         url: "https://github.com/imsyy/SPlayer",
+  //       },
+  //       {
+  //         icon: "https://pic.efefee.cn/uploads/2024/04/08/6613465358077.png",
+  //         name: "Snavigation",
+  //         url: "https://github.com/imsyy/SPlayer",
+  //       },
+  //       {
+  //         icon: "/images/logo/mfy_welcome.png",
+  //         name: "Home",
+  //         url: "https://github.com/imsyy/home",
+  //       },
+  //       {
+  //         icon: "https://pic.efefee.cn/uploads/2024/04/08/661346d418ad7.png",
+  //         name: "DailyHotApi",
+  //         url: "https://github.com/imsyy/DailyHotApi",
+  //       },
+  //       {
+  //         icon: "https://pic.efefee.cn/uploads/2024/04/08/66134722586fa.png",
+  //         name: "site-status",
+  //         url: "https://github.com/imsyy/site-status",
+  //       },
+  //     ],
+  //   },
+  // ],
   // 封面配置
   cover: {
     // 是否开启双栏布局
