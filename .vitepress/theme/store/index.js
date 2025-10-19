9abd9e62def99e6d5024ee6086f89af13975e268
dtrthg233 <dtrthg233@gmail.com>
2025-10-20 05:14:44 +0800

    mee

--- .vitepress/theme/store/index.js
+++ .vitepress/theme/store/index.js
@@ -51,7 +51,7 @@ export const mainStore = defineStore("main", {
       lastScrollY: 0,
       // 站点背景
       backgroundType: "patterns",
-      backgroundUrl: "https://tuapi.eees.cc/api.php?category={dongman,fengjing}&type=302",
+      backgroundUrl: "dtrthg233.github.io/public/images/logo/mfy_bg.webp",
     };
   },
   getters: {},
