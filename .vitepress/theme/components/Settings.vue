9ce22f751f1f669aa46e58ee9758de934d9f83e9
dtrthg233 <dtrthg233@gmail.com>
2025-11-12 19:19:19 +0800

    111

--- .vitepress/theme/components/Settings.vue
+++ .vitepress/theme/components/Settings.vue
@@ -16,23 +16,6 @@
       <div class="set-list">
         <span class="title">字体</span>
         <div class="set-item">
-          <span class="set-label">全站字体</span>
-          <div class="set-options">
-            <span
-              :class="['options', { choose: fontFamily === 'hmos' }]"
-              @click="fontFamily = 'hmos'"
-            >
-              HarmonyOS Sans
-            </span>
-            <span
-              :class="['options', { choose: fontFamily === 'lxgw' }]"
-              @click="fontFamily = 'lxgw'"
-            >
-              霞鹜文楷
-            </span>
-          </div>
-        </div>
-        <div class="set-item">
           <span class="set-label">全站字体大小</span>
           <div class="set-options">
             <span class="options" @click="store.changeFontSize(false)"> - </span>
